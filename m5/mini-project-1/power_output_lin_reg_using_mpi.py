59faf798efd02045a0f0e3f14b1df0204abdd218
Rajiv Ranjan <rajiv-ranjan@users.noreply.github.com>
2025-05-18 01:09:17 +0530

    completed m5 mp1 ex 1-17

--- m5/mini-project-1/power_output_lin_reg_using_mpi.py
+++ m5/mini-project-1/power_output_lin_reg_using_mpi.py
@@ -1,10 +1,35 @@
 
+from projects_functions_library import (
+    load_data,
+    clean_data,
+    scale_data,
+    split_data_into_X_and_Y,
+    estimate_coefficients,
+    FILENAME,
+)
+from utility import download_and_unzip
 from create_communicator import create_comm
 
 
 def main():
     comm, rank, size = create_comm()
+
+    download_and_unzip(
+        filename="PowerPlantData.csv",
+        url="https://cdn.iisc.talentsprint.com/CDS/Datasets/PowerPlantData.csv",
+    )
+
     if rank == 0:
+        df = load_data(FILENAME)
+
+        df = clean_data(df)
+
+        df = scale_data(df)
+
+        X, Y = split_data_into_X_and_Y(df)
+
+        coefficients = estimate_coefficients(X, Y)
+
         # Load the data
         # df = load_data(FILENAME)
         print(comm, rank, size)
--- m5/mini-project-1/projects_functions_library.py
+++ m5/mini-project-1/projects_functions_library.py
@@ -24,3 +24,73 @@ from sklearn.metrics import mean_squared_error
 from sklearn.utils import shuffle
 
 # import tensorflow as tf
+import nbformat
+
+
+from utility import download_and_unzip
+# FILENAME = "/content/PowerPlantData.csv" # File path
+FILENAME = "PowerPlantData.csv"  # File path
+
+
+# YOUR CODE HERE to Define a function to load the data
+# Function to load the data
+def load_data(file_path):
+    """
+    Function to load the data from the given file path
+    """
+    # Load the data into a pandas DataFrame
+    data = pd.read_csv(file_path, header=0, sep=",", engine="python")
+
+    # Renaming columns in a DataFrame
+    data.rename(
+        columns={
+            "AT": "AmbientTemperature",
+            "V": "ExhaustVaccum",
+            "AP": "AmbientPressure",
+            "RH": "RelativeHumidity",
+            "PE": "EnergyOutput",
+        },
+        inplace=True,
+    )
+    # Return the DataFrame
+    return data
+
+
+def clean_data(df):
+    """
+    Function to clean the data
+    """
+    print("before dropping missing values: {}".format(df.shape))
+    df_ = df.dropna().reset_index(drop=True)  # Drop rows with missing values
+    print("after dropping missing values {}".format(df_.shape))
+    df_ = df.drop_duplicates().reset_index(drop=True)
+    print("after dropping duplicate values {}".format(df_.shape))
+    return df_
+
+
+def scale_data(df):
+    """
+    Function to standardize the data
+    """
+    # Standardizing the data
+    df_ = (df - df.mean()) / df.std()
+    return df_
+
+
+def split_data_into_X_and_Y(df):
+    """
+    Function to split the data into X and Y
+    """
+    # Extracting the target column as Y and remaining columns as X
+    target_column = "EnergyOutput"  # Replace with the name of your target column
+    X = df.drop(columns=[target_column])  # Features
+    Y = df[target_column]  # Target
+
+    return X, Y
+
+
+def estimate_coefficients(X, Y):
+    # Calculate the coefficients using the formula
+    coefficients = np.linalg.inv(X.T @ X) @ X.T @ Y
+
+    return coefficients
